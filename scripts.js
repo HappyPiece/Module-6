8dbeab0256bd4ea1bfa40c08dc1b996349d775da
asyalox <arseniy.timchenko6@gmail.com>
2022-03-24 18:10:07 +0700

    made neural network recognize sus

--- scripts.js
+++ scripts.js
@@ -101,6 +101,9 @@ function getImage()
                 image.src = "./resources/gachimuchi/rambo.gif";
                 document.getElementById("content").append(image);
                 break;
+            case 5:
+                document.write("<script src = 'nn.js'></script>");
+                break;
             default:
                 image.src = "./resources/gachimuchi/gachi-fist.gif";
                 document.getElementById("content").append(image);
