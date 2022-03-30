6096e7b79fb9d61bad31b732a85e6ef4b15a2ac2
FnPngn AlexandroHIV <disturbedelk@gmail.com>
2022-03-30 10:38:40 +0700

    A* iterations per tick controls

--- scripts/astar.js
+++ scripts/astar.js
@@ -144,7 +144,7 @@ class Alg {
 
     step() {
         if (this.startingPoint == null | this.endingPoint == null) {
-            alert("specify starting and ending points first");
+            alert("Specify starting and ending points first");
             pause();
             return;
         }
@@ -169,38 +169,36 @@ class Alg {
             }
             return;
         }
-        if (dynamicSteps && this.open.length) {
-            algSteps = this.open.length - 1;
+        // if (dynamicSteps && this.open.length) {
+        //     algSteps = this.open.length - 1;
+        // }
+        if (this.open.length <= 0) {
+            this.startingPoint.value = globals.id.end;
+            pause();
+            document.getElementById('begin').changeText("Restart");
+            this.isFinished = true;
+            alert("Couldn't find path");
+            return;
         }
-        do {
-            if (this.open.length <= 0) {
-                let notfoundsound = new Audio(srcPath + '404.wav');
-                notfoundsound.play();
-                this.startingPoint.value = globals.id.end;
-                pause();
-                document.getElementById('begin').changeText("Restart");
-                this.isFinished = true;
-                alert("Couldn't find path");
-                return;
+        let foundIndex = 0;
+        for (let index = 1; index < this.open.length; ++index) {
+            if (this.open[index].cost < this.open[foundIndex].cost) {
+                foundIndex = index;
             }
-            let foundIndex = 0;
-            for (let index = 1; index < this.open.length; ++index) {
-                if (this.open[index].cost < this.open[foundIndex].cost) {
-                    foundIndex = index;
-                }
-            }
-            let current = this.open[foundIndex];
-            this.open.swapDelete(foundIndex);
-            current.isEvaluated = true;
+        }
+        let current = this.open[foundIndex];
+        this.open.swapDelete(foundIndex);
+        current.isEvaluated = true;
 
-            if (current.pos == this.endingPoint.pos) {
-                this.isFinished = true;
-                return;
-            }
-            this.iterateNeighbors(current);
-            current.value = globals.id.visited;
-            // this.open.sort((a, b) => a.cost - b.cost);
-        } while (this.stepCount++ % algSteps || this.isFinished);
+        if (current.pos == this.endingPoint.pos) {
+            this.isFinished = true;
+            return;
+        }
+        this.iterateNeighbors(current);
+        current.value = globals.id.visited;
+        // this.open.sort((a, b) => a.cost - b.cost);
+        this.stepCount++;
+
     }
 
 }
@@ -707,17 +705,14 @@ function initializeParams() {
     updateIntervalS.style.minWidth = "175px";
     let gridSizeS = createCustomSlider(1, 5, 'gridResize', "100%", Math.round(gridSize / 25), function () { resizeGrid(this.value < 5 ? Math.round(this.value * 25) : 600); }, (x) => gridSize);
     let begin = createCustomButton('begin', "Begin Pathfinding", "100%", onStartButton);
-    let test = createCustomSelectionWheel("seltest", "100%", ["1", "2", "penis"], testSelHandler);
     begin.style.marginTop = "5px";
     addParameter("Desired TPS", updateIntervalS);
     addParameter("Grid Size", gridSizeS);
+    addParameter("Steps Per Tick", createCustomSlider(1, 200, 'algsteps', "100%", algSteps, function () { algSteps = this.value; }, (x) => algSteps));
     addParameter(null, createCustomCheckbox('bloomCb', false, "Bloom", function () { this.checkbox.checked ? context.disableBloom() : context.enableBloom(); }));
     addParameter(null, createCustomCheckbox('generate', false, "Generate Maze", function () { this.checkbox.checked = true; alg.reset(); grid.clearGrid(); grid.genMaze(); }));
     addParameter(null, createCustomCheckbox('openDist', false, "Faster", function () { if (globals.paused == false) this.checkbox.checked = !this.checkbox.checked; else prioritizeOpenDistance = this.checkbox.checked; }));
     addParameter(null, begin);
-    addParameter(null, test);
-    addParameter("ASDFG:", createCustomNumberSelection("testns", "100%", 1, 0, 10, testNumSelHandler));
-
     document.getElementById('content').appendChild(parameterDiv);
 }
 
@@ -940,7 +935,9 @@ function draw() {
 }
 
 function update() {
-    alg.step();
+    do {
+        alg.step();
+    } while (alg.stepCount % algSteps && !alg.isFinished);
     draw();
 }
 
@@ -1237,8 +1234,6 @@ function start() {
     initialize();
     alg = new Alg();
     updateFunctionIntervalId = setInterval(draw, updateInterval);
-    let search = new Audio(srcPath + 'search.wav');
-    search.play();
 }
 
 function exit() {
